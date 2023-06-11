b0bf5acf0542cf246066f715e8675de4b967e03a
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-11 16:17:24 +0100

    commit

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 $(document).keypress(function(event){
-    $(".heroe2 > h1").text("Level One!")
-    var sound = new Audio();
-    var shouldPlay = true;
+    $(".heroe2 > h1").text("Level One!");
+    // var sound = new Audio();
+    // var shouldPlay = false;
     let counter = 1;
-    while(shouldPlay){
+    // while(shouldPlay){
         for(var i = 0; i < counter; i++){
             var number = Math.floor(Math.random() * 4);
             switch(number){
                 case 0:
-                    $(".green").animate({opacity : 0.9})
+                    $(".green").animate({opacity : 1}).delay(500).animate({opacity : 0.5})
                     break;
                 case 1:
-                    $(".red").animate({opacity : 0.9})
+                    $(".red").animate({opacity : 1}).delay(500).animate({opacity : 0.5})
                     break;
                 case 2:
-                    $(".yellow").animate({opacity : 0.9})
+                    $(".yellow").animate({opacity : 1}).delay(500).animate({opacity : 0.5})
                     break;
                 case 3:
-                    $(".blue").animate({opacity : 0.9})
+                    $(".blue").animate({opacity : 1}).delay(500).animate({opacity : 0.5})
                     break;
                 default:
                     break;
             }
         }
         counter++;
-    }
+    // }
 })
