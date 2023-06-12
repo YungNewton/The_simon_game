da785309e2f702e08544d6e06f0218250d2ef8d8
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-12 19:04:53 +0100

    commit

--- index.js
+++ index.js
@@ -13,25 +13,21 @@ $(document).keypress(function(){
                         var sound = new Audio('green.mp3');
                         sound.play();
                         $(".green").animate({opacity : 1}).delay(500).animate({opacity : 0.5})
-                        sleep(500);
                         break;
                     case 1:
                         var audio = new Audio('red.mp3');
                         audio.play();
                         $(".red").animate({opacity : 1}).delay(500).animate({opacity : 0.5})
-                        sleep(500);
                         break;
                     case 2:
                         var sound3 = new Audio('yellow.mp3');
                         sound3.play();
                         $(".yellow").animate({opacity : 1}).delay(500).animate({opacity : 0.5})
-                        sleep(500);
                         break;
                     case 3:
                         var sound4 = new Audio('blue.mp3');
                         sound4.play();
                         $(".blue").animate({opacity : 1}).delay(500).animate({opacity : 0.5})
-                        sleep(500);
                         break;
                     default:
                         break;
