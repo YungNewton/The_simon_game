fa340d28a019c6a386a0545eb1afee0405c8df38
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-12 21:32:13 +0100

    commit

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 
 $(document).keypress(function(){
     var number_array = [];
-    let counter = 1;
-    var should_continue = true;
-    setTimeout(()=>{
-        while(counter < 4){
+
+    for (let counter = 1; counter < 3; counter++) {
+        setTimeout(function timer() {
             var number = Math.floor(Math.random() * 4);
             number_array.push(number);
-            $(".heroe2 > h1").text("Level "+number_array.length);
+            $(".heroe2 > h1").text("Level " + number_array.length);
             for(var i = 0; i < number_array.length; i++){
                 switch(number_array[i]){
                     case 0:
                         setTimeout(()=>{
                             var sound = new Audio('green.mp3');
+                            $(".green").animate({opacity : 1}).delay(300).animate({opacity : 0.5})
                             sound.play();
-                            $(".green").animate({opacity : 1}).delay(500).animate({opacity : 0.5})}, 500);
+                        },i * 1500);
                         break;
                     case 1:
                         setTimeout(()=>{
                             var audio = new Audio('red.mp3');
+                            $(".red").animate({opacity : 1}).delay(300).animate({opacity : 0.5})
                             audio.play();
-                            $(".red").animate({opacity : 1}).delay(500).animate({opacity : 0.5})}, 500);
+                        },i * 1500);
                         break;
                     case 2:
                         setTimeout(()=>{
                             var sound3 = new Audio('yellow.mp3');
+                            $(".yellow").animate({opacity : 1}).delay(300).animate({opacity : 0.5})
                             sound3.play();
-                            $(".yellow").animate({opacity : 1}).delay(500).animate({opacity : 0.5})}, 500);
+                        },i * 1500);
                         break;
                     case 3:
                         setTimeout(()=>{
                             var sound4 = new Audio('blue.mp3');
+                            $(".blue").animate({opacity : 1}).delay(300).animate({opacity : 0.5})
                             sound4.play();
-                            $(".blue").animate({opacity : 1}).delay(500).animate({opacity : 0.5})}, 500);
+                        },i * 1000);
                         break;
                     default:
                         break;
                 }
             }
-            should_continue = false;
-            counter++;
-    
-        }
-    }, 500);
+        }, counter * 1000);
+    }
+
 })
